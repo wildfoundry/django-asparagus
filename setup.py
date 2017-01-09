cbf05540124317c6f2e40d3fa1586ab24e93905d
Elliot Mackenzie <emackenzie@dataplicity.com>
2017-01-09 17:56:01 +0000

    fixed typo

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/wildfoundry/django-asparagust',
+    url='https://github.com/wildfoundry/django-asparagus',
 
     # Author details
     author='Wildfoundry',
